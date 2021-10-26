a5205a551ff6bebabbbd56e0d6f67ec99c035269
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-26 18:07:19 +0200

    coding the thread checker

--- srcs/ft_run.c
+++ srcs/ft_run.c
@@ -6,11 +6,41 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 17:36:03 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/26 17:54:51 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/26 18:06:56 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	*ft_check_thread(void *a)
+{
+	t_p	*p;
+	int	i;
+
+	p = (t_p *)a;
+	while (1)
+	{
+		i = 0;
+		while (i < p->n_philo)
+		{
+			//pthread_mutex_lock p
+			if (*p->dead == p->n_philo * p->n_eat_max);
+				return (NULL);
+			//pthread_mutex_lock &p[i]
+			if (ft_get_time() - p[i].last_meal > p->ms_alive)
+			{
+				// print dead
+				*p->dead = TRUE;
+				return (NULL);
+			}
+			//pthread_mutex_unlock &p[i]
+			//pthread_mutex_unlock p
+			i++;
+		}
+		usleep(100);
+	}
+	return (NULL);
+}
 
 static void	ft_half_thread(t_p *p, int *dead, time_t time, int i)
 {
@@ -33,15 +63,15 @@ int	ft_run(t_p *p)
 	int			i;
 
 	dead = FALSE;
-	ft_half_thread(p, 0, &dead);
+	ft_half_thread(p, &dead, time, 0);
 	// usleep
-	ft_half_thread(p, 1, &dead);
-	// pthread_create(&thread, NULL, ft_???, p);
-	// pthread_join(thread, NULL);
+	ft_half_thread(p, &dead, time, 1);
+	pthread_create(&thread, NULL, ft_check_thread, p);
+	pthread_join(thread, NULL);
 	i = 0;
 	while (i < p->n_philo)
 	{
-		//pthread_detach(p[i].thread);
+		pthread_detach(p[i].thread);
 		i++;
 	}
 }
