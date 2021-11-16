3ddb13d655caafb0263dcc9d559e126dd6dfc07d
Alexdelia <alexandre.delille.57@gmail.com>
2021-11-16 12:46:34 +0100

    nor

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: adelille <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/24 16:56:43 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/28 13:41:43 by adelille         ###   ########.fr       */
+/*   Updated: 2021/11/16 12:46:20 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,10 @@ int	main(int ac, char **av)
 	pthread_mutex_t	*mutex;
 	int				i;
 
-	// eating -> sleeping -> thinking
-
-	// av: n_philo, ms_alive, ms_eating, ms_sleeping, optional: n_eating_for_each then stop
-
 	if (ft_arg(&a, ac, av) == FALSE)
 		return (1);
 	mutex = ft_init_mutex(&a);
 	p = ft_init_philo(&a, mutex);
-	// launch % 2 == 0 then == 1
-	// check if only one philosophers can crash the program
 	ft_run(p);
 	i = 0;
 	while (i < p->n_philo)
